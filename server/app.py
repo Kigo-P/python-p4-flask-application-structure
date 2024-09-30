0745c4a565e1b5c79aafe5cfab7ce98674e49019
Kigo-P <peterkigo33@gmail.com>
2024-09-30 21:51:53 +0300

    built a flask application

--- server/app.py
+++ server/app.py
@@ -3,3 +3,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+# creating a route for the index function/page
+@app.route("/")
+def index():
+    return "<h1>Welcome to my page!</h1>"
+
+#creating a dynamic application 
+# In the app.route surround the route with angle brackets <>
+#What is inside the angle brackets should be passed as the parameter for the function
+# You can specify the type in the angle brackets eg string, float, int e.t.c
+@app.route("/<string:username>")
+def user(username):
+    return f"<h1>Profile for {username}</h1>"
+
+
+# using the app.run() method
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
